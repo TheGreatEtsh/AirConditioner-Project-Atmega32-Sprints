b6833292a4d2cb3d4873467e91015617e76c0448
TheGreatEtsh <ahmedhesham606@icloud.com>
2023-04-18 16:50:15 +0200

    defining states for Application

--- AirConditioner/APP/app.h
+++ AirConditioner/APP/app.h
@@ -9,11 +9,17 @@
 #ifndef APP_H_
 #define APP_H_
 
-
-
 #include "../ECUAL/lm35/lm35.h"
 
+#define WELCOMING					0
+#define CHOOSING_TEMP				1
+#define IDLE_STATE					2
+#define RESETING_STATE				3
+
+
 void APP_initModules(void);
-void APP_superLoop (void);
+void APP_superLoop	(void);
+void APP_welcomingMsg (void);
+void APP_chooseTempMsg(void);
 
 #endif /* APP_H_ */
