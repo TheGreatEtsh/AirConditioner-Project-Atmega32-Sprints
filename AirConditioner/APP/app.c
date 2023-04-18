d15737d47bc542eca2c29833acb0ace866eb76bc
TheGreatEtsh <ahmedhesham606@icloud.com>
2023-04-18 04:30:23 +0200

    Adding APP driver

---
+++ AirConditioner/APP/app.c
@@ -0,0 +1,25 @@
+/*
+ * app.c
+ *
+ * Created: 4/18/2023 7:32:47 PM
+ *  Author: Ahmed Hesham
+ */ 
+
+#include "app.h"
+
+void APP_initModules(void)
+{
+	
+}
+
+
+
+void APP_superLoop (void)
+{
+	while (1)
+	{
+		
+	}
+}
+
+
---
+++ AirConditioner/APP/app.h
@@ -0,0 +1,19 @@
+/*
+ * app.h
+ *
+ * Created: 4/3/2023 7:32:34 PM
+ *  Author: Ahmed Hesham
+ */ 
+
+
+#ifndef APP_H_
+#define APP_H_
+
+
+
+#include "../ECUAL/lm35/lm35.h"
+
+void APP_initModules(void);
+void APP_superLoop (void);
+
+#endif /* APP_H_ */
