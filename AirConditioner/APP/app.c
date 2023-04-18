1ed840c7824a0adafbf9ac81fb1959557a5f42cd
TheGreatEtsh <ahmedhesham606@icloud.com>
2023-04-19 00:16:32 +0200

    Adding testing function for keypad driver in APP

--- AirConditioner/APP/app.c
+++ AirConditioner/APP/app.c
@@ -7,6 +7,9 @@
 
 #include "app.h"
 
+u8 u8_g_applicationState = 0;
+
+
 void APP_initModules(void)
 {
 	
@@ -16,9 +19,24 @@ void APP_initModules(void)
 
 void APP_superLoop (void)
 {
+	u8 keypadPressed = 0;
+	
 	while (1)
 	{
-		
+		switch(u8_g_applicationState)
+		{
+			case WELCOMING:	
+									break;
+			case CHOOSING_TEMP:					
+									break;
+			case IDLE_STATE:	
+									break;
+			case RESETTING_STATE:					
+									break;
+			
+			default:				
+									break;
+		}
 	}
 }
 
@@ -67,4 +85,40 @@ void APP_testingLm35(void)
 			DIO_write(PORT_B,0,LOW);
 		}
 	}	
+}
+
+void APP_testingKeypad(void)
+{
+	TIMER_init(TIMER_2);
+	TIMER_start(TIMER_2);
+	KEYPAD_init();
+	
+	DIO_init(PORT_B,0,OUTPUT);
+	DIO_init(PORT_B,1,OUTPUT);
+	DIO_init(PORT_B,2,OUTPUT);
+	DIO_init(PORT_B,3,OUTPUT);
+	
+	u8 KeyPressed = 0;							
+												
+	while(1)									
+	{											
+		KeyPressed = KEYPAD_read();				
+		while (!KeyPressed)						
+		{										
+			KeyPressed = KEYPAD_read();			
+			switch (KeyPressed)					
+			{
+				case '1':	DIO_write(PORT_B,3,LOW);	DIO_write(PORT_B,2,LOW);	DIO_write(PORT_B,1,LOW);	DIO_write(PORT_B,0,HIGH);	break;
+				case '2':	DIO_write(PORT_B,3,LOW);	DIO_write(PORT_B,2,LOW);	DIO_write(PORT_B,1,HIGH);	DIO_write(PORT_B,0,LOW);	break;
+				case '3':	DIO_write(PORT_B,3,LOW);	DIO_write(PORT_B,2,LOW);	DIO_write(PORT_B,1,HIGH);	DIO_write(PORT_B,0,HIGH);	break;
+				case '4':	DIO_write(PORT_B,3,LOW);	DIO_write(PORT_B,2,HIGH);	DIO_write(PORT_B,1,LOW);	DIO_write(PORT_B,0,LOW);	break;
+				case '5':	DIO_write(PORT_B,3,LOW);	DIO_write(PORT_B,2,HIGH);	DIO_write(PORT_B,1,LOW);	DIO_write(PORT_B,0,HIGH);	break;
+				case '6':	DIO_write(PORT_B,3,LOW);	DIO_write(PORT_B,2,HIGH);	DIO_write(PORT_B,1,HIGH);	DIO_write(PORT_B,0,LOW);	break;
+				case '7':	DIO_write(PORT_B,3,LOW);	DIO_write(PORT_B,2,HIGH);	DIO_write(PORT_B,1,HIGH);	DIO_write(PORT_B,0,HIGH);	break;
+				case '8':	DIO_write(PORT_B,3,HIGH);	DIO_write(PORT_B,2,LOW);	DIO_write(PORT_B,1,LOW);	DIO_write(PORT_B,0,LOW);	break;
+				case '9':	DIO_write(PORT_B,3,HIGH);	DIO_write(PORT_B,2,LOW);	DIO_write(PORT_B,1,LOW);	DIO_write(PORT_B,0,HIGH);	break;
+
+			}
+		}
+	}
 }
--- AirConditioner/APP/app.h
+++ AirConditioner/APP/app.h
@@ -10,13 +10,14 @@
 #define APP_H_
 
 #include "../ECUAL/lm35/lm35.h"
+#include "../ECUAL/keypad/keypad.h"
 #include "../MCAL/timer/timer.h"
 
 
 #define WELCOMING					0
 #define CHOOSING_TEMP				1
 #define IDLE_STATE					2
-#define RESETING_STATE				3
+#define RESETTING_STATE				3
 
 
 void APP_initModules(void);
@@ -29,6 +30,6 @@ void APP_chooseTempMsg(void);
 void APP_testingTimer(void);
 void APP_callBackTestingTask (void);
 void APP_testingLm35(void);
-
+void APP_testingKeypad(void);
 
 #endif /* APP_H_ */
